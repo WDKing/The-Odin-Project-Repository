566edf1650898e1087aee75bfc5b1902d3802b33
William King <Will.David.King@gmail.com>
2023-03-18 09:26:45 -0500

    Odin-Project basic information page index.js file

---
+++ odin-node-js-course/odin-basic-information-page/index.js
@@ -0,0 +1,45 @@
+const url = require('url');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = 'localhost';
+const port = 3250;
+
+const server = http.createServer((req, res) => {
+  const uripath = url.parse(req.url).pathname;
+  
+  switch (uripath) {
+    case '/':
+        readFile(res, './index.html');
+      break;
+    case '/about':
+        readFile(res, './about.html');
+      break;
+    case '/contact-me':
+        readFile(res,'./contact-me.html');
+      break;
+    default:
+        readFile(res, './404.html');
+      break;
+    };
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});   
+
+function readFile(res, filePath) {
+    const filePathConst = filePath;
+    console.log(String(filePath));
+    fs.readFile(String(filePath), 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.end(data);
+      });
+}
